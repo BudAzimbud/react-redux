35a1fe926dd46b2143071bb782ce837f3b9d2bb4
Budazimbud <budazimbud@gmail.com>
2022-02-09 08:47:46 +0700

    add navbar

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import NavbarComponent from './components/NavbarComponent';
+function App() {
+  return (
+    <div className="App">
+      <NavbarComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavbarComponent.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import {
+  Navbar,
+  DropdownItem,
+  NavLink,
+  NavbarText,
+  NavItem,
+  Nav,
+  Collapse,
+  NavbarBrand,
+  NavbarToggler,
+  DropdownToggle,
+  DropdownMenu,
+  UncontrolledDropdown,
+} from "reactstrap";
+export default function NavbarComponent() {
+  return (
+    <div>
+      <Navbar color="light" expand="md" light>
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={function noRefCheck() {}} />
+        <Collapse navbar>
+          <Nav className="me-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">
+                GitHub
+              </NavLink>
+            </NavItem>
+            <UncontrolledDropdown inNavbar nav>
+              <DropdownToggle caret nav>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Option 1</DropdownItem>
+                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}

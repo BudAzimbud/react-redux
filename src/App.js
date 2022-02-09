3301653e27cc51564a6c342edfae2a4674c0fd6c
Budazimbud <budazimbud@gmail.com>
2022-02-09 09:57:33 +0700

    adding table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
 import './App.css';
 import NavbarComponent from './components/NavbarComponent';
-function App() {
-  return (
-    <div className="App">
-      <NavbarComponent />
-    </div>
-  );
+import JumbrotonComponent from './components/JumbrotonComponent'
+import TableComponents from './components/TableComponents';
+import { Component } from 'react';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: 'title table',
+      users: [
+        {
+          id: 1,
+          nama: 'azim',
+          alamat: 'bandung',
+          umur: 19,
+          nohp: "0892384728347"
+        },
+        {
+          id: 2,
+          nama: 'zaim',
+          alamat: 'jakarta',
+          umur: 19,
+          nohp: "0892384728347"
+        },
+        {
+          id: 3,
+          nama: 'miza',
+          alamat: 'surabaya',
+          umur: 19,
+          nohp: "0892384728347"
+        }
+      ]
+    }
+  }
+
+
+  render() {
+
+    return (
+      <div className="App">
+        <NavbarComponent />
+        <JumbrotonComponent />
+        <TableComponents users={this.state.users} />
+      </div>
+    );
+
+  }
+
 }
 
 export default App;
